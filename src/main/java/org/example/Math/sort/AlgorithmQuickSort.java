260f4b0d6e358291963c98cf639202e1d1f89866
olegz <oleg.zylevich@hf-solution.com>
2025-07-05 15:35:03 +0300

    add quick sort using stack

--- src/main/java/org/example/Math/sort/AlgorithmQuickSort.java
+++ src/main/java/org/example/Math/sort/AlgorithmQuickSort.java
@@ -1,10 +1,14 @@
 package org.example.Math.sort;
+
+import java.util.LinkedList;
+
 public class AlgorithmQuickSort {
     public static int[] quickSort(int []array){
         if(array==null || array.length==0 || array.length==1) {
             return array;
         }
-        quickSort(array,0,array.length-1);
+        //quickSort(array,0,array.length-1);
+        quickSortUsingStack(array);
         return array;
     }
 
@@ -32,4 +36,47 @@ public class AlgorithmQuickSort {
                 quickSort(array,i,high);
         }
     }
+
+    public static int[] quickSortUsingStack(int[] array){
+        if(array==null || array.length==0 || array.length==1) {
+            return array;
+        }
+        LinkedList<Integer>stack=new LinkedList<>();
+        stack.add(array.length-1);
+        stack.add(0);
+        while (!stack.isEmpty()){
+            int high = stack.removeFirst();
+            int low = stack.removeFirst();
+
+            if (low >= high) {
+                continue;
+            }
+
+            int i=low;
+            int j=high;
+            int pivot = array[(i + j)/2];
+            while (i<=j){
+                while (array[i]<pivot)i++;
+                while (array[j]>pivot)j--;
+
+                if(i<=j){
+                    int temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                    i++;
+                    j--;
+                }
+            }
+            if(low<j) {
+                stack.addFirst(low);
+                stack.addFirst(j);
+            }
+            if(high>i) {
+                stack.addFirst(i);
+                stack.addFirst(high);
+            }
+        }
+        return array;
+    }
+
 }

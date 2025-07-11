e221d74ac4cfa88cf60cb9f215ef9295f13f0e87
oleg.zylevich <oleg.zylevich@OZYLEVICH>
2025-07-11 16:48:16 +0200

    fix merge sort and add test cases for method merge

--- src/main/java/org/example/Math/sort/nlogn/AlgorithmMergeSort.java
+++ src/main/java/org/example/Math/sort/nlogn/AlgorithmMergeSort.java
@@ -1,41 +1,73 @@
 package org.example.Math.sort.nlogn;
 
+import java.util.Arrays;
+
 public class AlgorithmMergeSort {
     public static int[] mergeSort(int []array){
         if(array == null || array.length == 0 || array.length == 1)
             return array;
-
+        int arraySize = array.length;
+        separateArray(array,0,arraySize-1);
         return array;
     }
-    public static int[] merge(int []array,int firstStart,int firstEnd,int secondStart,int secondEnd){
-        int currentPosition=firstStart;
-        int numberFromFirstPart=array[firstStart];
-        int numberFromSecondPart=array[secondStart];
-        int arrayResult[]=new int[array.length];
-        while(firstStart<=firstEnd || secondStart<=secondEnd){
-            if(secondStart<=secondEnd && numberFromFirstPart>numberFromSecondPart){
-                arrayResult[currentPosition]=numberFromSecondPart;
-                secondStart++;
-                if(secondStart<=secondEnd) {
-                    numberFromSecondPart = array[secondStart];
-                }
-                else{
-                    numberFromSecondPart=Integer.MAX_VALUE;
-                }
-            }
-            else if(firstStart<=firstEnd && numberFromFirstPart<numberFromSecondPart){
-                arrayResult[currentPosition]=numberFromFirstPart;
-                firstStart++;
-                if(firstStart<=firstEnd) {
-                    numberFromFirstPart = array[firstStart];
-                }
-                else{
-                    numberFromFirstPart=Integer.MAX_VALUE;
-                }
-            }
-            currentPosition++;
-        }
-        return arrayResult;
 
+    public static void separateArray(int[] array, int start, int end) {
+        if(start == end)
+            return;
+        int mid=(start+end)/2;
+        if(start<end) {
+            separateArray(array, start, mid);
+            separateArray(array, mid+1,end);
+            System.out.println("------------------------------------------");
+            for(int i=start; i<=end; i++){
+                System.out.print(array[i]+" ");
+            }
+            System.out.println("start:"+start+" mid:"+mid+" end:"+end);
+            System.out.println("------------------------------------------");
+            merge(array,start,mid,mid+1,end);
+        }
+        //
+    }
+
+    public static void merge(int []array,int firstStart,int firstEnd,int secondStart,int secondEnd){
+        if(array.length==0
+                || array.length==1
+                || firstStart>firstEnd
+                || secondStart>secondEnd
+        || firstStart>=secondStart
+                || firstEnd>=secondStart)
+            return ;
+        int currentResultPosition=0;
+        int currentFirstPosition=firstStart;
+        int currentSecondPosition=secondStart;
+        int arrayResult[]=new int[secondEnd-firstStart+1];
+        while(currentFirstPosition<=firstEnd && currentSecondPosition<=secondEnd
+                && currentSecondPosition<array.length
+                && currentFirstPosition<array.length){
+            if(array[currentFirstPosition]<array[currentSecondPosition]){
+                arrayResult[currentResultPosition]=array[currentFirstPosition];
+                currentFirstPosition++;
+            }
+            else{
+                arrayResult[currentResultPosition]=array[currentSecondPosition];
+                currentSecondPosition++;
+            }
+            currentResultPosition++;
+        }
+
+        while(currentFirstPosition<array.length && currentFirstPosition<=firstEnd){
+            arrayResult[currentResultPosition]=array[currentFirstPosition];
+            currentResultPosition++;
+            currentFirstPosition++;
+        }
+        while( currentSecondPosition<array.length && currentSecondPosition<=secondEnd){
+            arrayResult[currentResultPosition]=array[currentSecondPosition];
+            currentResultPosition++;
+            currentSecondPosition++;
+        }
+
+        for (int j=0,i = firstStart; i <= secondEnd; i++,j++) {
+           array[i] = arrayResult[j];
+        }
     }
 }
